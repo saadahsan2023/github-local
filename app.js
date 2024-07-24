0d67fb6984b0622f8917f718b36f6467942d9b8c
saadkhan2023 <saadahsankhan890@gmail.com>
2024-07-24 21:53:14 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const student = {
+    name: 'John Doe',
+    age: 25,
+    grade: 'A',
+    subjects: ['Math', 'Science', 'English']
+
+}
+
+const { name, grade, age } = student;
+
+console.log(`Name: ${name} , Grade: ${grade}, Age: ${age}`);
